1b6a9f011351c89612b0ecbf8f4720a4b6906d00
Madiyor <abdukhashimov@yandex.ru>
2024-10-23 16:25:47 +0500

    php warnings are not displayed

--- init.lua
+++ init.lua
@@ -1,2 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "php",
+  callback = function()
+    vim.diagnostic.enable(false) -- Disable diagnostics for current buffer
+  end,
+})
