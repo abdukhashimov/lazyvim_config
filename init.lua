e7b3e1211fa5112d69a4fc0945058accf5e8af27
Madiyor <abdukhashimov@yandex.ru>
2025-09-27 18:40:35 +0500

    fix: remove not needed theme and others

--- init.lua
+++ init.lua
@@ -8,4 +8,3 @@ vim.o.backup = false
 vim.o.writebackup = false
 vim.o.wrap = true -- Enable line wrapping
 vim.opt.wildignore:append({ "**/node_modules/**", "**/vendor/**" })
-vim.cmd.colorscheme("catppuccin")
